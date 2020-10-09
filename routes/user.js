3d63c74ccd0e0ab4bcdb8e3ebd176646b70d071b
daxindg <ksgwsxksg@gmail.com>
2020-10-09 21:22:21 +0800

    add change password function;fix searchbox focus

--- routes/user.js
+++ routes/user.js
@@ -3,51 +3,44 @@ var bcrypt = require('bcrypt');
 var router = express.Router();
 const { body, validationResult } = require('express-validator');
 const { sanitizeBody } = require('express-validator');
+const { errors } = require('pg-promise');
 /* GET users listing. */
 router.post('/login', [
-    body('username', 'invalid username').isLength({ min: 3, max: 10 }).trim().escape(),
-    body('password', 'invalid username').isLength({ min: 8, max: 20 }).trim().escape(),
     function (req, res) {
 
         if (req.session.user !== undefined) {
             res.send({ ok: true, data: req.session.user });
             return;
         }
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            req.session.dbconn.one('SELECT * FROM users WHERE name = $1', [req.body.username])
-                .then(
-                    data => {
-                        // console.log(data);
-                        if (bcrypt.compareSync(req.body.password, data.psw)) {
-                            console.log('ok');
-                            req.session.user = data;
-                            delete req.session.user.psw;
-                            // console.log(req.session.user);
-                            res.send({ ok: true, data: req.session.user });
-                        }
-                        else {
-                            res.send({ ok: false, err: { key: 'password', msg: '密码错误' } });
-                        }
 
+        req.session.dbconn.one('SELECT * FROM users WHERE name = $1', [req.body.username])
+            .then(
+                data => {
+                    // console.log(data);
+                    if (bcrypt.compareSync(req.body.password, data.psw)) {
+                        console.log('ok');
+                        req.session.user = data;
+                        delete req.session.user.psw;
+                        // console.log(req.session.user);
+                        res.send({ ok: true, data: req.session.user });
                     }
-                )
-                .catch(err => {
-                    // console.log('/router/user.js:31', err);
-                    res.send({ ok: false, err: { key: 'username', msg: '用户不存在' } });
-                })
-        }
-        else {
-            // console.log(errors.array());
-            res.send({ ok: false, err: errors.array() });
-        }
+                    else {
+                        res.send({ ok: false, err: { key: 'password', msg: '密码错误' } });
+                    }
+
+                }
+            )
+            .catch(err => {
+                // console.log('/router/user.js:31', err);
+                res.send({ ok: false, err: { key: 'username', msg: '用户不存在' } });
+            })
 
     }
 ]);
 
 router.post('/register', [
     body('email').isEmail().trim().escape().custom(async function (val, { req }) {
-        let {count} = await req.session.dbconn.one('SELECT count(email) FROM users WHERE email=$1', val);
+        let { count } = await req.session.dbconn.one('SELECT count(email) FROM users WHERE email=$1', val);
         // console.log('email', count);
         count = parseInt(count);
         if (count !== 0) {
@@ -58,7 +51,7 @@ router.post('/register', [
         return Promise.resolve();
     }),
     body('username', 'invalid username').isLength({ min: 3, max: 10 }).trim().escape().custom(async function (val, { req }) {
-        let {count} = await req.session.dbconn.one('SELECT count(name) FROM users WHERE name=$1', val);
+        let { count } = await req.session.dbconn.one('SELECT count(name) FROM users WHERE name=$1', val);
         count = parseInt(count);
         // console.log('username', count);
         if (count !== 0) {
@@ -72,13 +65,13 @@ router.post('/register', [
     function (req, res) {
         const errors = validationResult(req);
         if (errors.isEmpty()) {
-            
+
             const salt = bcrypt.genSaltSync();
             req.session.dbconn.one("INSERT INTO users VALUES(default, $1, $2, $3, 'user') RETURNING *", [req.body.email, req.body.username, bcrypt.hashSync(req.body.password, salt)])
                 .then(data => {
                     delete data.psw;
                     req.session.user = data;
-                    res.send({ok:true, data:data});
+                    res.send({ ok: true, data: data });
                 }
                 )
                 .catch(err => {
@@ -88,12 +81,55 @@ router.post('/register', [
 
         }
         else {
-            
-            res.send({ok:false, err:errors.array()});
+
+            res.send({ ok: false, err: errors.array() });
         }
 
     }
 ]);
+
+router.post('/chpsw', [
+    body('password', '密码长度 8-20').isLength({ min: 8, max: 20 }).trim().escape(),
+    (req, res) => {
+        if (req.session.user === undefined) {
+            res.send({ ok: false });
+            return;
+        }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.send({ ok: false, err: errors.array() });
+            return;
+        }
+        console.log(req.body);
+
+        req.session.dbconn.one('SELECT psw, uid FROM users WHERE uid = $1', [req.session.user.uid])
+            .then(
+                data => {
+                    // console.log(data);
+                    if (bcrypt.compareSync(req.body.password0, data.psw)) {
+                        const salt = bcrypt.genSaltSync();
+                        req.session.dbconn.none("UPDATE users SET psw = $1 WHERE uid = $2", [bcrypt.hashSync(req.body.password, salt), data.uid])
+                            .then(() => {
+                                res.send({ ok: true });
+                            }
+                            )
+                            .catch(err => {
+                                console.log(err);
+                                res.send({ ok: false, err: [] });
+                            });
+                    }
+                    else {
+                        console.log(data);
+                        res.send({ ok: false, err: [{ param: 'password0', msg: '密码错误' }] });
+                    }
+
+                }
+            )
+            .catch(err => {
+                console.log('/router/user.js:31', err);
+                res.send({ ok: false, err: [{ param: 'password0', msg: '请登录' }] });
+            });
+    }]);
 
 router.post('/logout', (req, res) => {
     req.session.destroy();
