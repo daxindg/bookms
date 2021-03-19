e788ffbaf3d82c7cfe84cd34ff5320f2a1709047
daxindg <my@example.com>
2021-03-19 21:25:37 +0800

    enable ssl for dbconn

--- db/dbconn.js
+++ db/dbconn.js
@@ -1,11 +1,12 @@
-var pgp = require('pg-promise')();
-
+var pgp = require("pg-promise")();
 
 if (process.env.PROD) {
-    exports.userconn = exports.guestconn = exports.adminconn = pgp(process.env.DATABASE_URL);
-}
-else {
-    exports.adminconn = pgp(process.env.DB_URL_ADMIN);
-    exports.userconn = pgp(process.env.DB_URL_USER);
-    exports.guestconn = pgp(process.env.DB_URL_GUEST);
+  exports.userconn = exports.guestconn = exports.adminconn = pgp({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+} else {
+  exports.adminconn = pgp(process.env.DB_URL_ADMIN);
+  exports.userconn = pgp(process.env.DB_URL_USER);
+  exports.guestconn = pgp(process.env.DB_URL_GUEST);
 }
